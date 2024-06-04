9499443156b88e06165ebeedf904da9737f600d5
KarlaSolano <36199935+KarlaSolano@users.noreply.github.com>
2024-06-04 02:46:56 -0600

    config(routes): se configuran las rutas para admin.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FormularioController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +23,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Añadimos Rutas para formularios
+    Route::post('/formulario', [FormularioController::class, 'store']);
+    Route::get('/formulario', [FormularioController::class, 'index'])->name('formulario.index');
+
 });
 
 require __DIR__.'/auth.php';
+require __DIR__.'/admin-auth.php'; //Agregar auth de admin
