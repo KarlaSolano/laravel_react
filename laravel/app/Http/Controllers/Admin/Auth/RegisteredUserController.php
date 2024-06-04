13a7f33b395a3350f81a18a45ad89d95cd12f2c4
KarlaSolano <36199935+KarlaSolano@users.noreply.github.com>
2024-06-04 02:46:08 -0600

    feat(admin): se integra controladores y Request para admin.

---
+++ laravel/app/Http/Controllers/Admin/Auth/RegisteredUserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class RegisteredUserController extends Controller
+{
+   
+    public function create(): Response
+    {
+        return Inertia::render('Admin/Auth/Register');
+    }
+
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'email' => 'nullable|string|lowercase|email|max:255',
+            'name' => 'required|string|max:255|unique:'.Admin::class,
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $admin = Admin::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($admin));
+
+        Auth::guard('admin')->login($admin);
+
+        return redirect(route('admin.dashboard'));
+    }
+}
